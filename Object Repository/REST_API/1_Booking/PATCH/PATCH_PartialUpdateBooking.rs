4f71431ceed9e72666dcc704bc0f172813774dac
Najwa <89608983+nakarz@users.noreply.github.com>
2023-09-15 13:50:07 +0700

    Develop to Master (#4)
    * Booking to Develop (#1)
    
    * feat: add object repositories for booking endpoint
    
    * feat: add test cases for booking endpoint
    
    * Auth to Develop (#2)
    
    * feat: add object repositories for booking endpoint
    
    * feat: add test cases for booking endpoint
    
    * feat: add object repositories for auth
    
    * feat: add test case for auth
    
    * Ping to Develop (#3)
    
    * feat: add object repositories for booking endpoint
    
    * feat: add test cases for booking endpoint
    
    * feat: add object repositories for auth
    
    * feat: add test case for auth
    
    * feat: add object repositories for ping
    
    * refactor: add test case for ping
    
    * refactor: add test suites
    
    * feat: add dummy test case from JIRA

---
+++ Object Repository/REST_API/1_Booking/PATCH/PATCH_PartialUpdateBooking.rs
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description>Updates a current booking with a partial payload</description>
+   <name>PATCH_PartialUpdateBooking</name>
+   <tag></tag>
+   <elementGuidId>90b471b0-5397-4ad7-a599-2fbd69834705</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <autoUpdateContent>false</autoUpdateContent>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;{\n    \&quot;firstname\&quot; : \&quot;James\&quot;,\n    \&quot;lastname\&quot; : \&quot;Brown\&quot;\n}&quot;,
+  &quot;contentType&quot;: &quot;application/json&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>application/json</value>
+      <webElementGuid>0c810045-787b-4096-9ea7-02fce997a44c</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Accept</name>
+      <type>Main</type>
+      <value>application/json</value>
+      <webElementGuid>14c643d4-0bf8-4362-bec8-6370d5987eeb</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Cookie</name>
+      <type>Main</type>
+      <value>token=abc123</value>
+      <webElementGuid>d5d1a7c6-7238-45a0-988c-379c5d4547be</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization</name>
+      <type>Main</type>
+      <value>Basic YWRtaW46cGFzc3dvcmQxMjM=</value>
+      <webElementGuid>404f0715-a238-4c9c-b8c6-90dca3f4ce97</webElementGuid>
+   </httpHeaderProperties>
+   <katalonVersion>8.6.5</katalonVersion>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>PATCH</restRequestMethod>
+   <restUrl>${GlobalVariable.baseURL}/booking/1</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+WS.verifyResponseStatusCode(response, 200)
+
+assertThat(response.getStatusCode()).isEqualTo(200)
+
+WS.verifyElementPropertyValue(response, 'firstname', &quot;James&quot;)
+WS.verifyElementPropertyValue(response, 'lastname', &quot;Brown&quot;)</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
